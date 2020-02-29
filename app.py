1a5d09cc2c79cd10e405ea3da3288feed49b37db
bestdragonkill <61057263+bestdragonkill@users.noreply.github.com>
2020-02-29 17:43:53 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+hours_now = 12
+minutes_now = 50
+second = 1
+number_of_hours_since_midnight = 12
+number_of_minutes_on_this_project = 5
+print(24 - hours_now)
+print(12* 50 /100)
+print(number_of_minutes_on_this_project)
+print(12 * 3600)
